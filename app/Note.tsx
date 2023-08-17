82a9b7a3e7c55e9d273c45293e281c9b417659db
Piklo <16057987+Piklo@users.noreply.github.com>
2023-08-17 18:57:24 +0200

    added edit button

--- app/Note.tsx
+++ app/Note.tsx
@@ -7,8 +7,11 @@ export interface INote {
 export function Note({ id, title, text }: INote) {
   return (
     <div className="container m-2 rounded border border-red-600">
-      <div className="bg-fuchsia-200">{title}</div>
-      <div>{text}</div>
+      <div className="flex flex-row bg-fuchsia-200">
+        <div className="container p-1">{title}</div>
+        <button className="self-end rounded bg-blue-500 p-1">Edit</button>
+      </div>
+      <div className="p-1">{text}</div>
     </div>
   );
 }
