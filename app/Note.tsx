264030959942a928062e5364865c93f0365bbe8a
Piklo <16057987+Piklo@users.noreply.github.com>
2023-08-17 19:02:45 +0200

    changed to grid

--- app/Note.tsx
+++ app/Note.tsx
@@ -6,7 +6,7 @@ export interface INote {
 
 export function Note({ id, title, text }: INote) {
   return (
-    <div className="container m-2 rounded border border-red-600">
+    <div className="container rounded border border-red-600">
       <div className="flex flex-row bg-fuchsia-200">
         <div className="container p-1">{title}</div>
         <button className="self-end rounded bg-blue-500 p-1">Edit</button>
--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,13 @@ export default function Page() {
     { id: "1", title: "my first note omegalul", text: "11111111" },
     { id: "2", title: "this shit kinda fun", text: "222222222" },
     { id: "3", title: "i hope i can get good eventually", text: "333333" },
+    { id: "4", title: "how do i exit vim", text: "i have absolutely no idea" },
+    { id: "5", title: "tom is okay", text: "sometimes anyway" },
+    { id: "6", title: "what do i do now", text: "im kinda out of ideas" },
+    { id: "7", title: "123123", text: "312322" },
   ];
   const items = notes.map((note) => (
     <Note key={note.id} id={note.id} text={note.text} title={note.title}></Note>
   ));
-  return <div className="flex flex-row">{items}</div>;
+  return <div className="grid grid-cols-3 gap-2 p-2">{items}</div>;
 }
