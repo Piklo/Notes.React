0cf70132fe606101dca11973040fa9dc3158b7a9
Piklo <16057987+Piklo@users.noreply.github.com>
2023-09-02 14:55:32 +0200

    fixed styling around edit button in Note added delete button

--- app/Note.tsx
+++ app/Note.tsx
@@ -6,10 +6,15 @@ export interface NoteProps {
 export function Note({ value: text }: NoteProps) {
   return (
     <div className="container rounded border border-red-600">
-      <div className="flex flex-row bg-fuchsia-200">
-        <button className="self-end rounded bg-blue-500 p-1">Edit</button>
-      </div>
       <div className="p-1">{text}</div>
+      <div>
+        <button className="w-1/2 self-end  bg-blue-300 p-1 hover:bg-blue-400">
+          Edit
+        </button>
+        <button className="w-1/2 self-end  bg-red-300 p-1 hover:bg-red-400">
+          Delete
+        </button>
+      </div>
     </div>
   );
 }
