8f7bf98f80645593481b894f0bda84812ac3d8f9
Piklo <16057987+Piklo@users.noreply.github.com>
2023-09-02 14:46:32 +0200

    updated NoreProps interface to match the response added basic fetching to the main page

--- app/Note.tsx
+++ app/Note.tsx
@@ -1,14 +1,12 @@
 export interface NoteProps {
   id: string;
-  title: string;
-  text: string;
+  value: string;
 }
 
-export function Note({ id, title, text }: NoteProps) {
+export function Note({ value: text }: NoteProps) {
   return (
     <div className="container rounded border border-red-600">
       <div className="flex flex-row bg-fuchsia-200">
-        <div className="container p-1">{title}</div>
         <button className="self-end rounded bg-blue-500 p-1">Edit</button>
       </div>
       <div className="p-1">{text}</div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,32 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { NoteProps, Note } from "./Note";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
-  const notes: NoteProps[] = [
-    { id: "1", title: "my first note omegalul", text: "11111111" },
-    { id: "2", title: "this shit kinda fun", text: "222222222" },
-    { id: "3", title: "i hope i can get good eventually", text: "333333" },
-    { id: "4", title: "how do i exit vim", text: "i have absolutely no idea" },
-    { id: "5", title: "tom is okay", text: "sometimes anyway" },
-    { id: "6", title: "what do i do now", text: "im kinda out of ideas" },
-    { id: "7", title: "123123", text: "312322" },
-  ];
+  const [notes, setNotes] = useState([] as NoteProps[]);
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const asd = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/getNotes`, {
+          credentials: "include",
+        });
+        const json = await asd.json();
+        console.dir(json);
+        setNotes(json.notes);
+      } catch (error) {
+        router.push("/login");
+      }
+    };
+
+    fetchData();
+  }, [router]);
+
   const items = notes.map((note) => (
-    <Note key={note.id} id={note.id} text={note.text} title={note.title}></Note>
+    <Note key={note.id} id={note.id} value={note.value}></Note>
   ));
   return <div className="grid grid-cols-3 gap-2 p-2">{items}</div>;
 }
