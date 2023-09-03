1a9a5beb42ae5e86feaf2fd05e402b4137d927f3
Piklo <16057987+Piklo@users.noreply.github.com>
2023-09-03 13:10:18 +0200

    added a simple navbar

---
+++ app/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <div className="flex">
+      <a href="/">
+        <div className="rounded border p-2 hover:bg-gray-200">Home</div>
+      </a>
+    </div>
+  );
+}
